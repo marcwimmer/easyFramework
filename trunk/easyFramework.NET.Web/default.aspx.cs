b0178ade707c122e5712ff915a18d1303a016c78
marc <marc@d3fae308-15bc-4758-8e8b-49114353db93>
2011-03-06 22:52:40 +0000

    git-svn-id: file:///home/marc/Arbeitsfläche/convert_svn/svn/easyframework@138 d3fae308-15bc-4758-8e8b-49114353db93

---
+++ trunk/easyFramework.NET.Web/default.aspx.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace easyFramework.Project.Surveymanager
+{
+	/// <summary>
+	/// Zusammenfassung für _default.
+	/// </summary>
+	public class _default : System.Web.UI.Page
+	{
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			// Hier Benutzercode zur Seiteninitialisierung einfügen
+		}
+
+		#region Vom Web Form-Designer generierter Code
+		override protected void OnInit(EventArgs e)
+		{
+			//
+			// CODEGEN: Dieser Aufruf ist für den ASP.NET Web Form-Designer erforderlich.
+			//
+			InitializeComponent();
+			base.OnInit(e);
+		}
+		
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung. 
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{    
+			this.Load += new System.EventHandler(this.Page_Load);
+		}
+		#endregion
+	}
+}
