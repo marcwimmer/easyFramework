201a9c2cde0aa5c10a804f0d421900bef9f81d9d
marc <marc@d3fae308-15bc-4758-8e8b-49114353db93>
2011-03-07 00:16:12 +0000

    git-svn-id: file:///home/marc/Arbeitsfläche/convert_svn/svn/easyframework@144 d3fae308-15bc-4758-8e8b-49114353db93

---
+++ trunk/easyFramework.NET/Tests/Tests/Test_DataMethodsClientInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using easyFramework.Sys.Data;
+using easyFramework.Sys;
+using System.Data.SqlClient;
+
+namespace Tests
+{
+    [TestFixture]
+    class Test_DataMethodsClientInfo
+    {
+        [Test]
+        public void TestSet1()
+        {
+            ClientInfo ci = new ClientInfo("CLIENT1", connstr());
+            {
+                string sql = "IF NOT EXISTS(SELECT * FROM sysobjects where name ='tt1') CREATE TABLE tt1 (f1 nvarchar(255));";
+                SqlConnection conn = new SqlConnection(connstr());
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            Recordset rs = DataMethodsClientInfo.gRsGetTable(ci, "tt1", "f1", ""); 
+        }
+
+        private string connstr()
+        {
+            return "data source=127.0.0.1;initial catalog=ef1;pwd=ppp22==;uid=sa;";
+        }
+    }
+}
---
+++ trunk/easyFramework.NET/Tests/Tests/Test_ToolLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using easyFramework.Sys.ToolLib;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Test_ToolLib
+    {
+        [Test]
+        public void Test_Left()
+        {
+            Assert.That(Functions.Left("12345", 3), Is.EqualTo("123"));
+            Assert.That(Functions.Left("12345", 6), Is.EqualTo("12345"));
+            Assert.That(Functions.Replace("12345", "3", "2"), Is.EqualTo("12245"));
+        }
+    }
+}
