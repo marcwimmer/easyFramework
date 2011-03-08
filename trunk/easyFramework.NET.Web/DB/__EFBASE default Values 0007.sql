cd6a5a24d49a65be60ebe14d6b5eced8cc43cac9
marc <marc@d3fae308-15bc-4758-8e8b-49114353db93>
2011-03-08 07:16:59 +0000

    git-svn-id: file:///home/marc/Arbeitsfläche/convert_svn/svn/easyframework@197 d3fae308-15bc-4758-8e8b-49114353db93

--- trunk/easyFramework.NET.Web/DB/__EFBASE default Values 0007.sql
+++ trunk/easyFramework.NET.Web/DB/__EFBASE default Values 0007.sql
@@ -4,34 +4,43 @@ delete from tsEntityTabs where tab_tabid = 'AbonnentPackages'
 GO
 DROP TABLE tsAbonnents
 GO
-ENTERPRISE_COPYPASTE:
-tsEntities
-	CONFIG	CONFIG		ASP/system/entityedit/entityedit.aspx?entity=Config&cfg_id=$1	/asp/system/config/dialog.xml	/asp/system/config/tableDef.xml	800	600	cfg_name;cfg_value	cfg_name		cfg_name + ISNULL(' ' + cfg_value, 'N/A')	0	cfg_name;cfg_value	Name;Wert	25%;75%
+insert into tsEntities(ety_name, ety_title, ety_searchdialog,ety_editdialog,ety_editdialogxml,  ety_tabledefxml, 
+  ety_windowwidth, ety_windowheight, ety_searchfields,  ety_primarysearchfield, 
+  ety_icon, ety_tostringfields, ety_concurrentupdates, ety_cols, ety_colcaptions, ety_colwidths)
+values('CONFIG', 'CONFIG', null, 'ASP/system/entityedit/entityedit.aspx?entity=Config&cfg_id=$1',
+'/asp/system/config/dialog.xml', 
+'/asp/system/config/tableDef.xml',
+'800', '600',
+'cfg_name;cfg_value',	'cfg_name',	null,	'cfg_name + ISNULL('' '' + cfg_value, ''N/A'')',	0,	'cfg_name;cfg_value',	'Name;Wert',	'25%;75%')
+
 GO
-ENTERPRISE_COPYPASTE:
-tsEntities
-	ABONNENTS	ABONNENTS		ASP/system/entityedit/entityedit.aspx?entity=ABONNENTS&abo_id=$1	/asp/system/abonnents/dialog.xml	/asp/system/abonnents/tableDef.xml	800	600	abo_name;abo_host	cfg_name		abo_name + ISNULL(' ' + abo_host, 'N/A')	0	abo_name;abo_host	Name;Host	25%;75%GO
+insert into tsEntities(ety_name, ety_title, ety_searchdialog, ety_editdialog, ety_editdialogxml, ety_tabledefxml, 
+  ety_windowwidth, ety_windowheight, ety_searchfields,  ety_primarysearchfield, 
+  ety_icon, ety_tostringfields, ety_concurrentupdates, ety_cols, ety_colcaptions, ety_colwidths)
+VALUES('ABONNENTS','ABONNENTS',	null,	'ASP/system/entityedit/entityedit.aspx?entity=ABONNENTS&abo_id=$1',	
+'/asp/system/abonnents/dialog.xml',
+'/asp/system/abonnents/tableDef.xml',
+800,600,
+'abo_name;abo_host', 'abo_name',null,'abo_name + ISNULL('' '' + abo_host, ''N/A'')',	0	,'abo_name;abo_host','Name;Host','25%;75%')
 GO
-ENTERPRISE_COPYPASTE:
-tsMainMenue
-	distribution		Distribution	''	1	0	*	treeview_folder	treeview_folder_open	0	
+INSERT INTO tsMainMenue(mnu_id, mnu_command, mnu_title, mnu_parentid, mnu_isfolder, mnu_modalwindow, mnu_edition, mnu_icon_normal, mnu_icon_opened, mnu_index)
+VALUES('distribution','','Distribution',null,1,0, '*', 'treeview_folder','treeview_folder_open',0)
+
 GO
-ENTERPRISE_COPYPASTE:
-tsMainMenue
-	distribution_edit	distribution	Abonnenten bearbeiten	/ASP/system/entityedit/entityedit.aspx?entity=ABONNENTS	0	0	*	treeview_item	treeview_item	0	
+INSERT INTO tsMainMenue(mnu_id, mnu_command, mnu_title, mnu_parentid, mnu_isfolder, mnu_modalwindow, mnu_edition, mnu_icon_normal, mnu_icon_opened, mnu_index)
+VALUES('distribution_edit','/ASP/system/entityedit/entityedit.aspx?entity=ABONNENTS','distribution','Abonnenten bearbeiten',0,	0,'*','treeview_item','treeview_item',0)
 GO
-ENTERPRISE_COPYPASTE:
-tsMainMenue
-	distribution_newpackage	distribution	Neues Paket erstellen	/ASP/tasks/distribution/newPackageDlg.aspx	0	0	*	treeview_item	treeview_item	1	
+INSERT INTO tsMainMenue(mnu_id, mnu_command, mnu_parentid, mnu_title, mnu_isfolder, mnu_modalwindow, mnu_edition, mnu_icon_normal, mnu_icon_opened, mnu_index)
+VALUES('distribution_newpackage','/ASP/tasks/distribution/newPackageDlg.aspx','distribution','Neues Paket erstellen',0,	0,'*','treeview_item','treeview_item',1	)
 GO
-ENTERPRISE_COPYPASTE:
-tsMainMenue
-	distribution_packages	distribution	Pakete anzeigen und verteilen	/ASP/system/entityedit/entityedit.aspx?entity=AbonnentPackages	0	0	*	treeview_item	treeview_item	2	
+
+INSERT INTO tsMainMenue(mnu_id, mnu_command, mnu_parentid, mnu_title, mnu_isfolder, mnu_modalwindow, mnu_edition, mnu_icon_normal, mnu_icon_opened, mnu_index)
+VALUES('distribution_packages','/ASP/system/entityedit/entityedit.aspx?entity=AbonnentPackages','distribution','Pakete anzeigen und verteilen', 0,0,'*','treeview_item','treeview_item',2)
 GO
-ENTERPRISE_COPYPASTE:
-tsEntityTabs
-		AbonnentPackages	AbonnentPackages	Abonnenten			/ASP/tasks/distribution/tabs/tab_abonennten.aspx	0
+INSERT INTO tsEntityTabs(tab_entity, tab_tabid, tab_tabtext, tab_url, tab_index)
+VALUES('AbonnentPackages','AbonnentPackages','Abonnenten','/ASP/tasks/distribution/tabs/tab_abonennten.aspx',0)
 GO
+
 CREATE TABLE [dbo].[tsAbonnents] (
 	[abo_id] [int] NOT NULL ,
 	[abo_name] [nvarchar] (128) NOT NULL ,
